fb38a852b9794f94a797b057025d5d1097599866
Pierina Ormando <110559878+pierinaormando@users.noreply.github.com>
2023-11-16 00:03:38 +0000

    prueba de fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,60 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
+
+	//crear usuario
+	fetch("https://playground.4geeks.com/apis/fake/todos/user/pierinaormando", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify([])
+	})
+		.then(response => response.json())
+		.then((data) => { console.log(data) })
+		.catch((err) => { console.log(err) })
+
+
+
+	/* fetch("https://playground.4geeks.com/apis/fake/todos/user/pierinaormando", {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify([
+				{ "label": "Make the bed", "done": false },
+				{ "label": "Walk", "done": true }
+			])
+		})
+			.then(response => response.json())
+			.then((data) => { console.log(data) })
+			.catch((err) => { console.log(err) }) */
+
+	const createTask = (label, done) => {
+		fetch("https://playground.4geeks.com/apis/fake/todos/user/pierinaormando", {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify([{ "label": label, "done": done }])
+		})
+			.then(response => response.json())
+			.then((data) => { console.log(data) })
+			.catch((err) => { console.log(err) })
+	};
+
+	createTask("Realizar práctica de fetch", true);
+
+	//leer las tareas
+	fetch("https://playground.4geeks.com/apis/fake/todos/user/pierinaormando")
+		.then(response => response.json())
+		.then(data => console.log(data))
+		.catch(error => console.error(error));
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center mt-5">
+			<h1>Practicando Fetch</h1>
 		</div>
 	);
 };
